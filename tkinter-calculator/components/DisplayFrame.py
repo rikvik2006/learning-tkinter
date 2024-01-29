b0b21644c9fe82bf5f75aec2a65ceb68cfaae899
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2024-01-29 18:21:19 +0100

    feat: Modifyed style

--- tkinter-calculator/components/DisplayFrame.py
+++ tkinter-calculator/components/DisplayFrame.py
@@ -7,16 +7,36 @@ class DisplayFrame(ttk.Frame):
         super().__init__(master, **kwargs)
         self.grid(**layout)
         self.create_display()
+        self.create_style()
         self.display.value.set("0")
 
     def create_display(self):
         self.display = CLabel(
             master=self,
             text="0",
-            anchor="ne",
+            anchor="nw",
             style="Display.TLabel",
         )
-        self.display.grid(row=0, column=0, sticky="nsew")
+        # Posiziona il widget nella posizione 0,0 del frame
+        self.display.grid(
+            row=0,
+            column=0,
+            sticky="nsew",
+        )
+        self.display.config(width=1)
+
+    def create_style(self):
+        # Imposta la grandezza della colonna 0 al 100% della grandezza del frame padre
+        self.grid_columnconfigure(0, weight=1)
+
+        self.style = ttk.Style()
+        self.style.theme_use("alt")
+        self.style.configure(
+            "Display.TLabel",
+            background="#1D1D1D",
+            foreground="#FFFFFF",
+            font=("Arial", 30),
+        )
 
     def add_text(self, text):
         current_text = self.display.value.get()
